3c33eb6f7deb2d1925c7d19a6db0b9c7082cbaa4
Aleksandr Saiapin <alstutor@gmail.com>
2022-02-27 17:28:35 +0700

    chore: Update dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.rst') as history_file:
 requirements = [
     "pydantic==1.9.0",
     "paho-mqtt==1.6.1",
-    "-e git+https://github.com/alsprogrammer/custom_logger.git#egg=custom_logger",
+    "custom_logger @ git+https://github.com/alsprogrammer/custom_logger.git#egg=custom_logger",
 ]
 
 test_requirements = [ ]
